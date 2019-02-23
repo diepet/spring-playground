e2d557dc1f6f87b6994ae226e9058b4f00786729
Diego Pettisani <diepet@gmail.com>
2019-02-23 23:16:27 +0100

    Added spring-hellojpa

---
+++ spring-hellojpa/src/main/java/it/diepet/spring/playground/hellojpa/HelloController.java
@@ -0,0 +1,15 @@
+package it.diepet.spring.playground.hellojpa;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
