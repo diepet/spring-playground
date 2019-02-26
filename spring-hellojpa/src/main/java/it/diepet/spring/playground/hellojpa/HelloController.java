ccba137e69381619249e4dfc9bc7df56c97c22c5
Diego Pettisani <diepet@gmail.com>
2019-02-26 19:31:25 +0100

    Added H2 configuration

--- spring-hellojpa/src/main/java/it/diepet/spring/playground/hellojpa/HelloController.java
+++ spring-hellojpa/src/main/java/it/diepet/spring/playground/hellojpa/HelloController.java
@@ -1,14 +1,33 @@
 package it.diepet.spring.playground.hellojpa;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.diepet.spring.playground.hellojpa.entity.Customer;
+import it.diepet.spring.playground.hellojpa.repository.CustomerRepository;
+
 @RestController
 public class HelloController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+	
+	@Autowired
+	private CustomerRepository customerRepository;
 
     @RequestMapping("/")
     public String index() {
+    	
+    	this.customerRepository.save(new Customer("AAA", "BBB"));
+    	
+    	Iterable<Customer> customers = this.customerRepository.findAll();
+    	for (Customer customer : customers) {
+    		LOGGER.info(customer.toString());
+    	}
+    	
         return "Greetings from Spring Boot!";
     }
 
--- spring-hellojpa/src/main/java/it/diepet/spring/playground/hellojpa/repository/CustomerRepository.java
+++ spring-hellojpa/src/main/java/it/diepet/spring/playground/hellojpa/repository/CustomerRepository.java
@@ -1,9 +1,13 @@
 package it.diepet.spring.playground.hellojpa.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import it.diepet.spring.playground.hellojpa.entity.Customer;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
+	 List<Customer> findByLastName(String lastName);
+	
 }
